1e7f75dd2d2ddc79bca75d4a3609f6f90d56cf50
jason <jasonyang1992@gmail.com>
2022-07-16 21:49:11 -0400

    second commit

--- src/main/java/Processor/PayCheckProcessor.java
+++ src/main/java/Processor/PayCheckProcessor.java
@@ -2,12 +2,20 @@ package Processor;
 
 import Metadata.CassandraMetadataReader;
 import Metadata.MetadataReader;
+import Util.FileSystemUtil;
+import Util.PropertyUtil;
+import Util.SparkUtil;
 import org.apache.spark.sql.Dataset;
 import org.apache.spark.sql.Row;
+import org.apache.spark.sql.types.StructField;
+import org.apache.spark.sql.types.StructType;
+
+import java.io.IOException;
+import java.util.List;
 
 public class PayCheckProcessor implements ApplicationProcessor{
 
-    private MetadataReader cassandraMetadataReader = new CassandraMetadataReader();
+    private final MetadataReader cassandraMetadataReader = new CassandraMetadataReader();
     private Dataset<Row> employees;
     private Dataset<Row> hourlyRates;
 
@@ -15,6 +23,14 @@ public class PayCheckProcessor implements ApplicationProcessor{
     public void startProcessor() {
         getTables();
 
+        Dataset<Row> joinSource = employees.join(hourlyRates, employees.col("title").equalTo(hourlyRates.col("title")));
+        joinSource = joinSource.select(employees.col("*"), hourlyRates.col("pay"));
+
+        try {
+            FileSystemUtil.writeToFile(joinSource, PropertyUtil.getProperty("File.Output.Location"), PropertyUtil.getProperty("File.Employee.Payroll"));
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     @Override
